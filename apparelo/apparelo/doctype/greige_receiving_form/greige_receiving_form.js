fdcd29bd052f1fd15acb657f53bc19da38c9f29e
smehata <smehata@dexciss.com>
2020-11-05 10:05:50 +0530

    updated

--- apparelo/apparelo/doctype/greige_receiving_form/greige_receiving_form.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, Aerele Technologies Private Limited and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Greige Receiving Form', {
-	// refresh: function(frm) {
-
-	// }
-});
